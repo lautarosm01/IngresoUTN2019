da8a54b8d5e7c53b48bb7721a36bc03c428b76d8
lautarosm01 <47337954+lautarosm01@users.noreply.github.com>
2019-02-04 22:03:56 -0300

    Add files via upload
    Ejercicios Lunes 4 de Febrero

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,23 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function Mostrar()
 {
-	
+	var nombre;
+
+    nombre = document.getElementById("elNombre").value;
+
+    alert("tu nombre es "+nombre);
+
 }
 
+/* Se puede hacer de varias maneras
+     var nombre;
+     var mensaje;
+
+     mensaje = "nombre es ";
+    
+    nombre = document.getElementById("elNombre").value;
+
+    alert( mensaje + "" + nombre) */
+    
+    
 
