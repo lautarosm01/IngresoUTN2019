ec115381896e8408cea1927507544e8b798f22af
lautarosm01 <47337954+lautarosm01@users.noreply.github.com>
2019-02-11 22:26:06 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,17 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarDescuento()
 {
-	
+	var precio;
+    var descuento;
+    var resultado; 
+
+    precio = parseFloat (document.getElementById("importe").value);
+
+    descuento = precio * 25 / 100;
+
+    resultado = precio - descuento;
+
+    alert("El descuento es de $"+ resultado);
+ 
+    document.getElementById("resultado").value = resultado;
 }
