1edc9d3dcf1d419e8c7e1520801a531c87b32123
lautarosm01 <47337954+lautarosm01@users.noreply.github.com>
2019-02-13 22:19:36 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -2,5 +2,29 @@ function Mostrar()
 {
 //tomo la edad  
 
+/*    var edad
 
-}//FIN DE LA FUNCIÓN
+    edad = parseInt (document.getElementById ("edad").value)
+
+    if ( !(edad >= 13 && edad <= 17)){
+
+        alert("No es Adolescente")
+    }
+*/
+
+    var edad;
+
+    edad= parseInt(document.getElementById("edad").value);
+
+    if( (edad < 13 || edad > 17)){
+
+        alert("No es Adolescente");}
+    //FIN DE LA FUNCIÓN 
+
+
+
+
+
+
+
+}
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,20 @@ function Mostrar()
 {
 //tomo la edad  
 
+    var edad;
 
+    edad = parseInt( document.getElementById("edad").value);
+
+    if (edad >= 18) {
+
+        alert("Es mayor")
+    } 
+    else if (edad >= 13 && edad <= 17) {
+        alert("Es adolescente")
+    }
+    else {
+        alert("Es Niño")
+    }
 
 
 
